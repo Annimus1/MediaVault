3edf3303b34826e71906d5afee67384e9b460a2e
Pablo Vergara <pablo.j98@gmail.com>
2025-03-25 21:54:26 -0400

    Add: types

---
+++ src/utils/enums.ts
@@ -0,0 +1,13 @@
+export enum Language {
+    "Spanish" = "spanish",
+    "English" = "english",
+    "SubJap" = "sub-spanish"
+}
+
+export enum mediaType {
+    "Movie" = "movie",
+    "Serie" = "serie",
+    "Anime" = "anime",
+    "VideoGame" = "videogame",
+    "Book" = "book"
+}
---
+++ src/utils/types.d.ts
@@ -0,0 +1,29 @@
+import { Language, mediaType } from "./enums.js"
+
+export type User = {
+    user: string,
+    email: string,
+    password: string
+}
+
+export type UserDTO = Omit<User, password, email>
+
+export interface Media {
+    id: string,
+    owner: UserDTO,
+    name: string,
+    completedDate: Date,
+    score: Number,
+    comment?: string,
+    poster: string,
+    mediaType: mediaType,
+    language: Language,
+}
+export type MediaSchema = Omit<Media, id>
+
+export interface IAuthToken extends Document {
+  owner: Types.ObjectId;  // Referencia al usuario
+  token: string;          // Token JWT
+  createdAt: Date;        // Fecha de creación (auto)
+  expiresAt: Date;        // Fecha de expiración
+}
