4d99e5594471a45d909b5d901bd048d7c071f39c
Pablo Vergara <pablo.j98@gmail.com>
2025-03-25 21:54:04 -0400

    Refactor: server app
    Separate router and app.

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { app } from "./app.js";
+import cookieParser from 'cookie-parser';
+import { dbConnection } from './db/mongo.js'
+
+dbConnection();
+const PORT = process.env.PORT || 5000;
+
+app.disable('x-powered-by');
+app.use(cookieParser())
+
+app.listen(PORT, () => {
+    console.log("Server running on port:", PORT)
+})
