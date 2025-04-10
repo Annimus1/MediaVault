22939b59faf94e35c93f594851f909ccb1e04348
Pablo Vergara <pablo.j98@gmail.com>
2025-04-10 17:13:09 -0400

    Refactor(build): Delete dist

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import authRoutes from './routes/auth.route.js';
-import mediaRoutes from './routes/media.route.js';
-const app = express();
-app.use(express.json());
-app.use('/api/v1', authRoutes);
-app.use('/api/v1', mediaRoutes);
-export { app };
