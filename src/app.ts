81c01e503c6f42e7c94d092c016b9e8bd8573f2c
Pablo Vergara <pablo.j98@gmail.com>
2025-06-16 11:16:34 -0400

    add: Cors config

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
-import express from "express";
+import express from 'express';
 import authRoutes from './v1/routes/auth.route.js';
 import mediaRoutes from './v1/routes/media.route.js';
+import cors from 'cors'
 
 const app = express();
+
+// Cors config
+app.use(cors({
+    origin: 'https://media-vault-ui.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 app.use('/api/v1', authRoutes);
